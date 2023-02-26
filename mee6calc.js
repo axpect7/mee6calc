379086ad49332b3c29e504f07829da406a6279ac
axpect7 <78979674+axpect7@users.noreply.github.com>
2023-02-26 17:50:13 +0530

    Update mee6calc.js

--- mee6calc.js
+++ mee6calc.js
@@ -5,16 +5,20 @@ function mee6calculator()
 
     var desiredlvl = document.getElementById("desired-level").value;
     var currentlvl = document.getElementById("current-lvl").value;
-    var current_xp = document.getElementById("current-xp").value;
+    var xpinlvl = document.getElementById("current-xp").value;
     //Magical math functions
     
-    var currentxp = (5/6 * currentlvl * (2 * currentlvl * currentlvl + 27 * currentlvl + 91)) + current_xp;
-    var desiredxp = 5/6 * desiredlvl * (2 * desiredlvl * desiredlvl + 27 * desiredlvl + 91) - currentxp;
+    var currentxp = 5/6 * currentlvl * (2 * currentlvl * currentlvl + 27 * currentlvl + 91);
+    var totalxp = parseInt(currentxp) + parseInt(xpinlvl);
+    var desiredxp = 5/6 * desiredlvl * (2 * desiredlvl * desiredlvl + 27 * desiredlvl + 91);
+    var totaldesiredxp = parseInt(desiredxp) - parseInt(totalxp);
 
     //Minimum, average, and maximum messages needed to send in order to reach specified level
-    var minmsg = Math.ceil(desiredxp / maxxp);
-    var avgmsg = Math.ceil(desiredxp / ((minxp + maxxp) / 2));
-    var maxmsg = Math.ceil(desiredxp / minxp);
+    var minmsg = Math.ceil(totaldesiredxp / maxxp);
+    var avg = parseInt(minxp) + parseInt(maxxp);
+    var avgxp = avg/2
+    var avgmsg = Math.ceil(totaldesiredxp / avgxp);
+    var maxmsg = Math.ceil(totaldesiredxp / minxp);
 
     //Display information to user
     document.getElementById("result-average").innerHTML = avgmsg;
